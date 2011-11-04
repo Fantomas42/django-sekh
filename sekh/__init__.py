2d5d8bb6da933eb515c447fb778f481f5d120ed6
Fantomas42 <fantomas42@gmail.com>
2011-11-04 14:44:55 +0100

    review setup

---
+++ sekh/__init__.py
@@ -0,0 +1,8 @@
+"""Search Engine Keywords Highlighter"""
+__version__ = '0.2.dev'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'https://github.com/Fantomas42/django-sekh'
--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
-from distutils.core import setup
 import os
+from setuptools import setup, find_packages
 
-version = '0.1'
+import sekh
 
-setup(name='django-sekh',
-      version=version,
-      
-      description='Highlight the keywords of a page if a visitor is coming form a search engine.',
-      long_description=open(os.path.join('README.rst')).read(),
-      keywords='django, search engine, keyword, highlight',
+setup(
+    name='django-sekh',
+    version=sekh.__version__,
 
-      author='Fantomas42',
-      author_email='fantomas42@gmail.com',
-      url='http://github.com/Fantomas42/django-sekh',
-      license='BSD License',
+    description='Highlight the keywords of a page if a visitor ' \
+    'is coming form a search engine.',
+    long_description=open(os.path.join('README.rst')).read(),
+    keywords='django, search engine, keyword, highlight',
 
-      packages=['sekh',],
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Environment :: Web Environment',
-          'Programming Language :: Python',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'License :: OSI Approved :: BSD License',
-          'Topic :: Software Development :: Libraries :: Python Modules',],
-      )
+    author=sekh.__author__,
+    author_email=sekh.__email__,
+    url=sekh.__url__,
+
+    packages=find_packages(),
+    classifiers=[
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',],
+
+    license=sekh.__license__,
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['BeautifulSoup>=3.2.0']
+    )
 
 
 
