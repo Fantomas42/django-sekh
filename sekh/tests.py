51b9cbaf2ca0e94cee27046df6e5817986093c0f
Fantomas42 <fantomas42@gmail.com>
2013-06-13 18:28:00 +0200

    Refactor tests

--- sekh/tests.py
+++ sekh/tests.py
@@ -3,15 +3,61 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.http import HttpResponse
 
+from sekh.utils import highlight
+from sekh.utils import remove_duplicates
 from sekh.middleware import KeywordsHighlightingMiddleware
 
-HTML_CONTENT = """
-<html>
-  <body>
-    <p>Hello world !</p>
-  </body>
-</html>
-"""
+
+HTML_CONTENT = '<html><body><p>Hello world !</p></body></html>'
+
+
+class TestRemoveDuplicates(TestCase):
+    """Tests for sekh.utils"""
+
+    def test_remove_duplicates(self):
+        self.assertEquals(
+            remove_duplicates(['titi', 'toto', 'tata', 'titi']),
+            ['titi', 'toto', 'tata'])
+
+
+class TestHighlight(TestCase):
+
+    def test_highlight(self):
+        self.assertEquals(
+            highlight(HTML_CONTENT, ['Hello', 'world']),
+            '<html><body><p><span class="highlight term-1">Hello</span> '
+            '<span class="highlight term-2">world</span> !</p></body></html>')
+
+    def test_highlight_case(self):
+        self.assertEquals(
+            highlight(HTML_CONTENT, ['HELLO', 'World']),
+            '<html><body><p><span class="highlight term-1">Hello</span> '
+            '<span class="highlight term-2">world</span> !</p></body></html>')
+
+    def test_highlight_none(self):
+        self.assertEquals(
+            highlight(HTML_CONTENT, []), HTML_CONTENT)
+
+    def test_highlight_multiple_in_one_markup(self):
+        self.assertEquals(
+            highlight(HTML_CONTENT.replace('!', 'hello'), ['hello']),
+            '<html><body><p><span class="highlight term-1">Hello</span> '
+            'world <span class="highlight term-1">hello</span></p>'
+            '</body></html>')
+
+    def test_dont_highlight_highlightings(self):
+        self.assertEquals(
+            highlight(HTML_CONTENT.replace('world', 'highlight'),
+                      ['hello', 'highlight']),
+            '<html><body><p><span class="highlight term-1">Hello</span> '
+            '<span class="highlight term-2">highlight</span> !</p>'
+            '</body></html>')
+
+    def test_dont_highlight_protected_markups(self):
+        self.assertEquals(
+            highlight(HTML_CONTENT.replace('world', '<pre>world</pre>'),
+                      ['world']),
+            '<html><body><p>Hello <pre>world</pre> !</p></body></html>')
 
 
 class KeywordsHighlightingMiddlewareTestCase(TestCase):
@@ -26,11 +72,6 @@ class KeywordsHighlightingMiddlewareTestCase(TestCase):
 
     def test_with_REFERER(self):
         response = KeywordsHighlightingMiddleware().process_response(
-            self._get_request(),
-            HttpResponse(HTML_CONTENT))
-        self.assertEquals(response.content, HTML_CONTENT)
-
-        response = KeywordsHighlightingMiddleware().process_response(
             self._get_request(referer='http://toto.com/?q=Hello'),
             HttpResponse(HTML_CONTENT))
         self.assertEquals(response.content, HTML_CONTENT)
@@ -38,8 +79,9 @@ class KeywordsHighlightingMiddlewareTestCase(TestCase):
         response = KeywordsHighlightingMiddleware().process_response(
             self._get_request(referer='http://www.google.com/?q=world'),
             HttpResponse(HTML_CONTENT))
-        self.assertTrue(
-            '<span class="highlight term-1">world</span>' in response.content)
+        self.assertEquals(response.content,
+                          '<html><body><p>Hello <span class="highlight '
+                          'term-1">world</span> !</p></body></html>')
 
     def test_with_GET(self):
         response = KeywordsHighlightingMiddleware().process_response(
@@ -55,54 +97,17 @@ class KeywordsHighlightingMiddlewareTestCase(TestCase):
         response = KeywordsHighlightingMiddleware().process_response(
             self._get_request({'highlight': 'Hello world'}),
             HttpResponse(HTML_CONTENT))
-        self.assertTrue(
-            '<span class="highlight term-1">Hello</span> '
-            '<span class="highlight term-2">world</span>' in response.content)
+        self.assertEquals(
+            response.content,
+            '<html><body><p><span class="highlight term-1">Hello</span> '
+            '<span class="highlight term-2">world</span> !</p></body></html>')
 
         response = KeywordsHighlightingMiddleware().process_response(
             self._get_request({'highlight': 'Hello',
                                'hl': 'World',
                                'q': 'Hello'}),
             HttpResponse(HTML_CONTENT))
-        self.assertTrue(
-            '<span class="highlight term-1">Hello</span> '
-            '<span class="highlight term-2">world</span>' in response.content)
-
-    def test_dont_highlight_highlightings(self):
-        response = KeywordsHighlightingMiddleware().process_response(
-            self._get_request({'highlight': 'Hello high'}),
-            HttpResponse(HTML_CONTENT))
-        self.assertTrue(
-            '<span class="highlight term-1">Hello</span>' in response.content)
-
-        response = KeywordsHighlightingMiddleware().process_response(
-            self._get_request({'highlight': 'Hello high'}),
-            HttpResponse(HTML_CONTENT.replace('world', 'high')))
-        self.assertTrue(
-            '<span class="highlight term-1">Hello</span> '
-            '<span class="highlight term-2">high</span>' in response.content)
-
-    def test_multiple_in_one_markup(self):
-        response = KeywordsHighlightingMiddleware().process_response(
-            self._get_request({'highlight': 'Hello'}),
-            HttpResponse('<html><body><p>Hello world hello !'
-                         '</p></body></html>'))
-        self.assertTrue(
-            '<span class="highlight term-1">Hello</span> world '
-            '<span class="highlight term-1">hello</span>' in response.content)
-
-    def test_with_cases_REFERER(self):
-        response = KeywordsHighlightingMiddleware().process_response(
-            self._get_request(referer='http://www.google.com/?q=HELLO World'),
-            HttpResponse(HTML_CONTENT))
-        self.assertTrue(
-            '<span class="highlight term-1">Hello</span> '
-            '<span class="highlight term-2">world</span>' in response.content)
-
-    def test_with_cases_GET(self):
-        response = KeywordsHighlightingMiddleware().process_response(
-            self._get_request({'highlight': 'HELLO World'}),
-            HttpResponse(HTML_CONTENT))
-        self.assertTrue(
-            '<span class="highlight term-1">Hello</span> '
-            '<span class="highlight term-2">world</span>' in response.content)
+        self.assertEquals(
+            response.content,
+            '<html><body><p><span class="highlight term-1">Hello</span> '
+            '<span class="highlight term-2">world</span> !</p></body></html>')
